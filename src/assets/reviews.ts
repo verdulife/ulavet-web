d6e8147ecae156fc7b9a0eb47d4a8635c03883f7
verdulife <verdu@live.com>
2023-11-26 19:09:58 +0100

    contact 2

--- src/assets/reviews.ts
+++ src/assets/reviews.ts
@@ -14,9 +14,16 @@ export const reviews: Array<Item> = [
   {
     avatar: "/avatars/raquel-bergua.png",
     name: "Raquel Bergua",
-    review: `Un encanto de centro, por el trato , la profesionalidad y el precio.
-    Llevo a mis 2 mascotas, perro y gato.
-    Tanto Laura como Úrsula son excepcionales. Te explican todo súper bien, y tratan con un cariño a los animales que está claro que tienen una profesión vocacional.
-    La zona no está mal para aparcar, hay un área de casitas delante en la que siempre encuentro un hueco.`,
+    review: `Un encanto de centro, por el trato , la profesionalidad y el precio. Llevo a mis 2 mascotas, perro y gato. Tanto Laura como Úrsula son excepcionales. Te explican todo súper bien, y tratan con un cariño a los animales que está claro que tienen una profesión vocacional. La zona no está mal para aparcar, hay un área de casitas delante en la que siempre encuentro un hueco.`,
+  },
+  {
+    avatar: "/avatars/txell-borras-capdevila.png",
+    name: "Txell Borràs Capdevila",
+    review: `Úrsula ha sido nuestra veterinaria desde siempre, y con esfuerzo y dedicación ha conseguido abrir una clínica que trata a nuestras mascotas con el amor y cariño que necesitan, asimismo estableciendo una relación muy cercana y empática con los propietarios. Su maravilloso trabajo salvó la vida a nuestro perro en más de una ocasión y estaremos eternamente agradecidos. Una clínica increíble que recomendaría a todo el mundo.`,
+  },
+  {
+    avatar: "/avatars/monica-sanchez.png",
+    name: "Mónica Sánchez",
+    review: `Llevamos a Bach desde hace casi 2 años y estamos encantados con Laura y Úrsula por su profesionalidad. Diagnósticos precisos y sin sobre-medicación para nuestros peludos . De hecho bastantes  de nuestros amigos llevamos todos allí a los perretes y somos de Cunit (+20 min. De coche)`,
   },
 ];
