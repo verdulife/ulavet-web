2c91f4a4e9d5433aa96194773e969f145d1a0d83
verdulife <verdu@live.com>
2024-11-23 12:13:06 +0100

    add ga

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from 'astro/config';
+
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+
+const pt_opts = {
+  config: {
+    forward: ["dataLayer.push"]
+  }
+};
 
 export default defineConfig({
   site: "https://www.ulavet.com",
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), partytown(pt_opts)]
 });
