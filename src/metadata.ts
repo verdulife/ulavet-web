fba575397154bf4d958bc75e529848e7454dd33d
verdulife <verdu@live.com>
2024-11-24 10:24:23 +0100

    add site name

--- src/metadata.ts
+++ src/metadata.ts
@@ -1,5 +1,5 @@
 export const site = {
-  name: "ULAVET",
+  name: "Ulavet",
   title: "ULAVET | Clinica veterinaria a Sant Pere de Ribes",
   description:
     "Atenció completa per a gats i gossos amb servei local i venda de productes veterinaris a Sant Pere de Ribes, Barcelona. Confia en nosaltres per al benestar de les teves mascotes!",
